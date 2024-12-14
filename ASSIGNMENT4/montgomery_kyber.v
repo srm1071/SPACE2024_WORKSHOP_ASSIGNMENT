92688242389608e4467c9dc89e6a6ad55e5352d0
srm1071 <36409839+srm1071@users.noreply.github.com>
2024-12-14 14:23:12 +0530

    Update montgomery_kyber.v

--- ASSIGNMENT4/montgomery_kyber.v
+++ ASSIGNMENT4/montgomery_kyber.v
@@ -24,6 +24,12 @@ input [11:0] a,b;
 output [11:0] out;
 wire [23:0] res;
 reg [23:0] res_t;
+wire [35:0] restd;
+wire [11:0] m;
+wire [23:0] inter_n;
+wire [24:0] resk;
+wire [12:0] resu;
+wire [12:0] modprime;
 wire [11:0] prime;
 assign prime=12'd3329;
 
@@ -31,6 +37,12 @@ assign res=a*b;
 always@(posedge clk) begin
     res_t<=res;
 end
-//write your code here
+assign restd=res_t*12'd3327;
+assign m=restd[11:0];
+assign inter_n=m*prime;
+assign resk=res_t+inter_n;
+assign resu=resk>>12;
+assign modprime=resu-prime;
+assign out=modprime[12]?resu[11:0]:modprime[11:0];
 
 endmodule
